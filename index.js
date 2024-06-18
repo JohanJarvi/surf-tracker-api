318a69625e8d6d0bd5246eeba3cd51f5ea85b029
Johan Jarvi <johan.jarvi@gmail.com>
2024-06-18 10:59:23 +1000

    Improve the API and add a remove option

--- index.js
+++ index.js
@@ -11,27 +11,61 @@ app.get("/", (req, res) => {
   const auth = req.headers["authorization"];
   const matches = auth === process.env.USER_AUTH;
 
+  console.log("Performing healthcheck");
+
   if (matches) {
     res.json({
       message: "Working correctly",
     });
+    console.log("Health check finished");
+  } else {
+    res.status(401);
+    res.json({ message: "Forbidden" });
+    console.log("Health check failed");
+  }
+});
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+app.post("/insert", async (req, res) => {
+  const auth = req.headers["authorization"];
+  const matches = auth === process.env.USER_AUTH;
+
+  console.log("Inserting new surf day");
+
+  if (matches) {
+    try {
+      console.log("Connecting to db");
+      await client.connect();
+      console.log("Inserting record");
+      await client.db("surfdays").collection("days").insertOne(req.body);
+      res.json({ message: "Inserted record", data: req.body });
+      console.log(`Successfully inserted record '${JSON.stringify(req.body)}'`);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      client.close();
+    }
   } else {
     res.status(401);
     res.json({ message: "Forbidden" });
   }
 });
 
-const client = new MongoClient(process.env.MONGODB_URI);
-
-app.post("/add", async (req, res) => {
+app.post("/delete", async (req, res) => {
   const auth = req.headers["authorization"];
   const matches = auth === process.env.USER_AUTH;
 
+  console.log(`Deleting surf day ${JSON.stringify(req.body)}`);
+
   if (matches) {
     try {
+      console.log("Connecting to db");
       await client.connect();
-      await client.db("surfdays").collection("days").insertOne(req.body);
-      res.json(req.body);
+      console.log("Deleting record");
+      await client.db("surfdays").collection("days").deleteMany(req.body);
+      res.json({ message: "Deleted record", data: req.body });
+      console.log(`Successfully deleted record '${JSON.stringify(req.body)}'`);
     } catch (err) {
       console.error(err);
     } finally {
